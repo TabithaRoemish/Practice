95b3e574ef7a73cf0f84cf3549b1724b23844698
Tabitha Roemish <tabitharoemish@gmail.com>
2017-06-15 14:33:03 -0700

    add practice file

---
+++ Practice/Practice/practice.cpp
@@ -0,0 +1,47 @@
+//kata practice space
+#include<vector>
+#include<iostream>
+#include<string>
+bool isAscOrder(std::vector<int> arr);
+
+//int main()
+//{
+//	int arr[] = { 3,2,1 };
+//	int arr1[] = { 1,2,3 };
+//	int arr2[] = { 1, 4, 2 };
+//
+//	std::vector<int> array0(arr, arr + sizeof(arr)/sizeof(int));
+//	std::vector<int> array1(arr1, arr1 + sizeof(arr1) / sizeof(int));
+//	std::vector<int> array2(arr2, arr2 + sizeof(arr2) / sizeof(int));
+//
+//	bool assertFalse = isAscOrder(array0);
+//	bool assertTrue = isAscOrder(array1);
+//	bool assertFalse2 = isAscOrder(array2);
+//	std::string sassertFalse = (assertFalse == false) ? "False" : "True";
+//	std::string sassertTrue = (assertTrue == true) ? "True" : "False";
+//	std::string sassertFalse2 = (assertFalse2 == false) ? "False" : "True";
+//
+//
+//	std::cout << "assertFalse: " << sassertFalse << std::endl;
+//	std::cout << "assertTrue: " << sassertTrue << std::endl;
+//	std::cout << "assertFalse2: " << sassertFalse2 << std::endl;
+//
+//	return 0;
+//}
+//
+//
+//bool isAscOrder(std::vector<int> arr)
+//{
+//	std::vector<int>::iterator it = arr.begin();
+//	std::vector<int>::iterator end = arr.end();
+//	end--;
+//	while (it != arr.end())
+//	{
+//		if(it != end)
+//			it++;
+//		if (*it < *(it - 1))
+//			return false;
+//	}
+//
+//	return true;
+//}
---
+++ Practice/Practice/tower.cpp
@@ -0,0 +1,45 @@
+//kata practice space
+#include<vector>
+#include<iostream>
+#include<string>
+std::vector<std::string> towerBuilder(int nFloors);
+
+int main()
+{
+	std::vector<std::string> tower = towerBuilder(6);
+
+	for (int i = 0; i < tower.size(); i++)
+		std::cout << tower[i] << std::endl;
+
+	return 0;
+}
+
+
+std::vector<std::string> towerBuilder(int nFloors)
+{
+	std::string tempstar = "";
+	std::string tempspace = "";
+	std::string tempString = "";
+
+	int max = 1;
+	int spacecount = nFloors;
+	std::vector<std::string> myVector;
+
+	for (int i = 1; i <= nFloors; i++)
+	{
+		spacecount--;
+		max = i + i - 1;
+		for (int m = 0; m < spacecount; m++)
+			tempspace += " ";
+		for (int j = max; j> 0; j--)
+		{
+			tempstar += "*";
+		}
+		tempString = tempspace + tempstar + tempspace;
+		myVector.push_back(tempString);
+		tempString = "";
+		tempstar = "";
+		tempspace = "";
+	}
+	return myVector;
+}
